cc8dc56656b0f7a5502ebdebdfa4c0f7457089ae
6ppng <hiroroppon@gmail.com>
2023-06-03 10:18:43 +0000

    [add] template

---
+++ README.md
@@ -0,0 +1,13 @@
+# 概要
+
+このリポジトリはPythonプロジェクトのテンプレートです。  
+Docker + poetryによるPython仮想環境のひな形を提供します。  
+初回構築手順は以下に従ってください。
+
+* [環境構築手順](/doc/環境構築手順.md)
+
+## 参考資料
+
+* [poetry](https://python-poetry.org/)
+* [Docker docs](https://docs.docker.com/)
+* [VS Code Remote Development](https://code.visualstudio.com/docs/remote/remote-overview)
---
+++ src/sample.py
@@ -0,0 +1,29 @@
+"""Main mod."""
+import os
+import pathlib
+
+
+def main() -> str:
+    """Do main logic."""
+    string = (
+        "atnisutauratsuasiuatsnuhogehgious"
+        + "iantisoutarsasntiroasnritrsonuaa"
+    )
+    print(string)
+    if "" == "":
+        if "" == "":
+            pass
+
+    print(
+        "hello world hello worldhello worldhello auauuaworldhello worldhello woaad"
+    )
+
+    return "hoge"
+
+
+if __name__ == "__main__":
+    i = "123"
+    i = main()
+
+    j = 123
+    j = 123
